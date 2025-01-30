d6732368c8cea1aa44f0ebff70c04a1b3de26cad
BonjourBenjamin <asejmbl@gmail.com>
2025-01-30 21:24:26 +0100

    $~1

--- pusher.bat
+++ pusher.bat
@@ -1,19 +1,26 @@
 @ECHO OFF
 SETLOCAL
 
-if "%~1"=="" (
-    ECHO "Usage: npm run send -- <message> <branch> [origin]"
+SET arg1="%~1"
+SET arg2="%~2"
+
+ECHO Argument 1: %arg1%
+ECHO Argument 2: %arg2%
+
+IF "%~1" EQU "" (
+    ECHO "Usage: npm run send -- <message> <branch> [origin :: 'origin']"
     EXIT /b 1
 )
-if "%~2"=="" (
-    ECHO "Usage: npm run send -- <message> <branch> [origin]"
+IF "%~2" EQU "" (
+    ECHO "Usage: npm run send -- <message> <branch> [origin :: 'origin']"
     EXIT /b 1
 )
 
-SET MESSAGE=$~1
-SET BRANCH=$~2
-SET ORIGIN=$~3
-IF "%ORIGIN%"=="" SET ORIGIN=origin
+SET "MESSAGE=$~1"
+SET "BRANCH=$~2"
+SET "ORIGIN=$~3"
+
+IF "%ORIGIN%" EQU "" SET ORIGIN=origin
 
 git add .
 git commit -m "%MESSAGE%"
