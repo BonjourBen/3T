2506fbcc5e76982e2e00e4a766d0e076ff8e8444
BonjourBenjamin <asejmbl@gmail.com>
2025-01-30 21:30:36 +0100

    fix pusher.bat

--- pusher.bat
+++ pusher.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
 SETLOCAL
 
-ECHO "%~1"
-ECHO "%~2"
-ECHO "%~3"
-
 IF "%~1" EQU "" (
     ECHO "Usage: npm run send -- <message> <branch> [origin -> 'origin']"
     EXIT /b 1
@@ -18,10 +14,6 @@ IF "%~2" EQU "" (
 SET MESSAGE=%~1
 SET BRANCH=%~2
 SET ORIGIN=%~3
-
-ECHO "%MESSAGE%"
-ECHO "%BRANCH%"
-ECHO "%ORIGIN%"
 
 IF "%ORIGIN%" EQU "" SET "ORIGIN=origin"
 
